ec1c30eee51c85560051cfb803766a5cf3bf5567
Khaled Abdallah <khaledabdalah488@gmail.com>
2025-06-05 15:01:27 +0300

    add migrations tables

---
+++ app/Models/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
+
+class Comment extends Model
+{
+    /** @use HasFactory<\Database\Factories\CommentFactory> */
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'content', 'approved'];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
