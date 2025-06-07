3d7b47139c8a803ede30f1f8e51879974d4fff4a
Mahmoud <Mahmoud@gmail.com>
2025-06-07 18:25:27 +0300

    fix savePost issue

--- app/Livewire/Navigations/SavedPosts.php
+++ app/Livewire/Navigations/SavedPosts.php
@@ -2,15 +2,17 @@
 
 namespace App\Livewire\Navigations;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use App\Models\User;
 
 #[Layout('components.layouts.app')]
 class SavedPosts extends Component
 {
     public function render ()
     {
-        $userSavedPosts = auth()->user()->savedPosts()->latest()->get();
+        $userSavedPosts = optional(Auth::user())->savedPosts()?->latest()?->get() ?? collect();
         return view('livewire.navigations.saved-posts', ['savedPosts' => $userSavedPosts]);
     }
 }
