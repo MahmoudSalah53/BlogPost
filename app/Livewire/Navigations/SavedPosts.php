1ded42df00c8ab3ffc5bbb1b1f1388f52e598500
Mahmoud <Mahmoud@gmail.com>
2025-06-07 18:44:49 +0300

    return

--- app/Livewire/Navigations/SavedPosts.php
+++ app/Livewire/Navigations/SavedPosts.php
@@ -2,17 +2,15 @@
 
 namespace App\Livewire\Navigations;
 
-use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
-use App\Models\User;
 
 #[Layout('components.layouts.app')]
 class SavedPosts extends Component
 {
     public function render ()
     {
-        $userSavedPosts = optional(Auth::user())->savedPosts()?->latest()?->get() ?? collect();
+        $userSavedPosts = auth()->user()->savedPosts()->latest()->get();
         return view('livewire.navigations.saved-posts', ['savedPosts' => $userSavedPosts]);
     }
 }
