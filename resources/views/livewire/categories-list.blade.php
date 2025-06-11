5f7676527416e43ce05498252fd0e9d277b8503c
Mahmoud <Mahmoud@gmail.com>
2025-06-11 10:31:32 +0300

    edit design of dark mode

--- resources/views/livewire/categories-list.blade.php
+++ resources/views/livewire/categories-list.blade.php
@@ -25,11 +25,11 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             @forelse($categories as $category)
-            <div class="bg-white dark:bg-zinc-800 rounded-xl shadow-sm p-6 text-center">
+            <div class="bg-white dark:bg-zinc-700 rounded-xl shadow-sm p-6 text-center">
                 <flux:text as="h3" size="xl" class="font-semibold text-zinc-800 dark:text-white mb-2">
                     {{ $category->name }}
                 </flux:text>
-                <flux:badge color="blue">
+                <flux:badge>
                     37
                 </flux:badge>
             </div>
@@ -40,7 +40,7 @@
             @endforelse
         </div>
 
-        <div class="mt-4">
+        <div class="mt-6">
             {{ $categories->links() }}
         </div>
     </div>
