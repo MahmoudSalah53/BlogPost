89c77efd845fc83b6c4dcc80884a34fb3043b447
Khaled Abdallah <khaledabdalah488@gmail.com>
2025-06-20 18:05:33 +0300

    fix follow issue

--- resources/views/livewire/follow-author-component.blade.php
+++ resources/views/livewire/follow-author-component.blade.php
@@ -1,13 +1,8 @@
-@if($isFollowing)
-    <div wire:key="unfollow">
-        <flux:button size="sm" wire:click="save" class="rounded-full ml-4">
-            Unfollow
-        </flux:button>
-    </div>
-@else
-    <div wire:key="follow">
-        <flux:button variant="primary" size="sm" wire:click="save" style="border-radios: 100%;">
-            Follow
-        </flux:button>
-    </div>
-@endif
+<flux:button
+    wire:click="save"
+    class="rounded-full ml-4"
+    variant="primary"
+    size="sm"
+>
+    {{ $isFollowing ? 'Unfollow' : 'Follow' }}
+</flux:button>
