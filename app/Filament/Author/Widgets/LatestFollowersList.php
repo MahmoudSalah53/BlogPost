8de7fd3102d234af1c5ff4c3ee58bcc1ba16f13b
Khaled Abdallah <khaledabdalah488@gmail.com>
2025-07-23 21:25:19 +0300

    make a latest followers list component for author panel

---
+++ app/Filament/Author/Widgets/LatestFollowersList.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Author\Widgets;
+
+use Filament\Widgets\Widget;
+
+class LatestFollowersList extends Widget
+{
+    protected static string $view = 'filament.author.widgets.latest-followers-list';
+}
