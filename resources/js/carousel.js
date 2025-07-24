994378df0dd6f2934fe390b5a8dea235ef1821f5
Khaled Abdallah <khaledabdalah488@gmail.com>
2025-07-24 14:48:02 +0300

    fix carousel issue when using wire navigation

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -1,31 +1,48 @@
 // import Swiper core , navigation, pagination
 import Swiper from "swiper";
-import {Autoplay, Navigation, Pagination} from "swiper/modules";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
 // import css
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination, Autoplay],
-    loop: true,
-    speed: 500,
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    spaceBetween: 20,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        640: {slidesPerView: 1},
-        768: {slidesPerView: 2},
-        1024: {slidesPerView: 3},
-    },
-    autoHeight: true,
-    autoplay: {
-        delay: 5000,
-    },
+let swiperInstance = null;
 
+function initSwiper() {
+
+    if (swiperInstance) {
+        swiperInstance.destroy(true, true);
+        swiperInstance = null;
+    }
+
+    swiperInstance = new Swiper('.swiper', {
+        modules: [Navigation, Pagination, Autoplay],
+        loop: true,
+        speed: 500,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+        },
+        autoHeight: true,
+        autoplay: {
+            delay: 5000,
+        },
+
+    });
+}
+
+document.addEventListener('DOMContentLoaded', initSwiper);
+
+document.addEventListener('livewire:navigated', () => {
+
+    setTimeout(initSwiper, 100);
 });
